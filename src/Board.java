16832287dc8cc97790ea2711996094670688a6ea
MoniBohm <monic.bohm@gmail.com>
2021-05-19 18:37:16 +0200

    [Refactore] toSting method

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,4 @@
 import java.awt.Color;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 
@@ -9,6 +7,7 @@ public class Board {
     private Pawn[][] fields;
     private int n;
 
+    //board creation
     public Board(int n) {
         this.n = n;
         //between 10 and 20, place pawn
@@ -41,7 +40,12 @@ public class Board {
         return color;
     }
 
-    public void removePawn(Pawn pawn) {
+    public String toString(){
+        StringGenerator boardString = new StringGenerator(fields);
+        return boardString.toString();
+    }
+    // move on board
+    private void removePawn(Pawn pawn) {
         int[] removeIndex = pawn.getCoordinates();
         fields[removeIndex[0]][removeIndex[1]] = null;
     }
@@ -56,11 +60,11 @@ public class Board {
             removePawn(pawn);
             //put new Pawn
             fields[coordinates[0]][coordinates[1]] = new Pawn(color, position, isCrowned);
-            enemyCaptured(pawn, coordinates);
+            collectEnemyAround(pawn, coordinates);
         }
     }
 
-    private void enemyCaptured(Pawn pawn, int[] endPosition) throws InterruptedException {
+    private void collectEnemyAround(Pawn pawn, int[] endPosition) throws InterruptedException {
         int [] startPosition = pawn.getCoordinates();
         if(Math.abs(startPosition[0] - endPosition[0]) == 2){
             if(startPosition[1] - endPosition[1] == -2){
@@ -96,99 +100,31 @@ public class Board {
         Pawn pawnOnEndPosition = fields[endPosition[0]][endPosition[1]];
         int optinalMove = nextCoordinate[0].length;
         switch (optinalMove){
-            case 0:
+            case 0: // no enemy around pawn
                 break;
-            case 2:
+            case 2: // 1 enemy around pawn
                 doAutomaticJump(pawnOnEndPosition, nextCoordinate[0]);
                 break;
-            default:
-//                chooseFromTheseCoordinates(nextCoordinate, pawn);
+            default: // more than 1 enemy around pawn
+                chooseFromTheseCoordinates(nextCoordinate, pawn);
                 break;
 
         }
-    }
-
-    private void chooseFromTheseCoordinates(int[] nextCoordinate, Pawn pawn) {
-        Game game = new Game();
-        game.automaticMoveManage(nextCoordinate);
     }
 
     private void doAutomaticJump(Pawn pawn,int[] coordinates) throws InterruptedException {
         movePawn(pawn, coordinates);
-        //new boardPrint with delay --ebben még nem vagyok biztos hogy így lesz teljesen :D
         System.out.println("Automatic jump!");
         TimeUnit.SECONDS.sleep(1);
     }
 
-    public String toString() {
-        final String newLine = "\n";
-        StringBuilder board = new StringBuilder();
-        StringBuilder header = createHeader();
-        board.append(header);
-        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        for (int i = 0; i < fields.length; i++) {
-            String letter = "\u001B[35m" + alphabet[i] + " ";
-            board.append(letter);
-            rowToString(board, fields[i], i);
-            board.append(newLine);
-        }
-        return board.toString();
+    private void chooseFromTheseCoordinates(int[][] nextCoordinate, Pawn pawn) {
+        Game game = new Game();
+        game.automaticMoveManage(nextCoordinate);
     }
-
-    private void rowToString(StringBuilder board, Pawn[] row, int rowNumber) {
-
-        final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-        final String ANSI_RESET = "\u001B[0m";
-        final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-        final String ANSI_GREEN_BACKGROUND = "\033[42m";
-        final String YELLOW_BACKGROUND = "\033[43m";
-
-        final String emptyBlackField = ANSI_BLACK_BACKGROUND+"    "+ANSI_RESET;
-        final String emptyWhiteField = ANSI_WHITE_BACKGROUND+"    "+ANSI_RESET;
-        final String blackPawnBlackField = ANSI_BLACK_BACKGROUND+" \uD83E\uDD81 "+ANSI_RESET;
-        final String whitePawnBlackField = ANSI_BLACK_BACKGROUND+" \uD83E\uDD92 "+ANSI_RESET;
-        // elephant: final String blackPawnBlackField = ANSI_BLACK_BACKGROUND+" \uD83D\uDC18 "+ANSI_RESET;
-
-        if(rowNumber%2==0){ // even rows
-            for(int i=0; i<row.length; i++){
-                if(i%2==0){ // black fields
-                    if(row[i] == null ){
-                        board.append(emptyBlackField);
-                    } else {
-                        String pawnAndField = (row[i].getColor() == Color.black ? blackPawnBlackField : whitePawnBlackField);
-                        board.append(pawnAndField);
-                    }
-                } else { // white fields
-                        board.append(emptyWhiteField);
-                }
-            }
-        } else { // odd rows
-            for(int i=0; i<row.length; i++){
-                if(i%2!=0){ // black fields
-                    if(row[i] == null ){
-                        board.append(emptyBlackField);
-                    } else {
-                        String pawnAndField = (row[i].getColor() == Color.black ? blackPawnBlackField : whitePawnBlackField);
-                        board.append(pawnAndField);
-                    }
-                } else { // white fields
-                        board.append(emptyWhiteField);
-                    }
-                }
-            }
-        }
 
     public Pawn getPawn(int row, int col){
         return fields[row][col];
     }
 
-    private StringBuilder createHeader() {
-        int width = fields[0].length;
-        StringBuilder header = new StringBuilder("  ");
-        for (int i = 1; i <= width; i++) {
-            String element = (i<10) ? "  "+ "\u001B[35m" + i + " " : " "+ "\u001B[35m" + i + " ";
-            header.append(element);
-        }
-        return header.append("\n");
-    }
 }
