6ee6f560fe7aea9b669e5b5d4a6686a399163913
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-05-19 21:06:00 +0200

    [Add] yellow color

--- src/StringGenerator.java
+++ src/StringGenerator.java
@@ -8,6 +8,7 @@ public class StringGenerator {
     final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
     final String ANSI_GREEN_BACKGROUND = "\033[42m";
     final String YELLOW_BACKGROUND = "\033[43m";
+    final String YELLOW = "\033[0;33m";
 
     final String emptyBlackField = ANSI_BLACK_BACKGROUND + "    " + ANSI_RESET;
     final String emptyWhiteField = ANSI_GREEN_BACKGROUND + "    " + ANSI_RESET;
@@ -25,7 +26,7 @@ public class StringGenerator {
         StringBuilder header = createHeader();
         board.append(header);
         for (int i = 0; i < fields.length; i++) {
-            String letter = "\u001B[35m" + alphabet[i] + " ";
+            String letter = YELLOW + alphabet[i] + " ";
             board.append(letter);
             rowToString(board, fields[i], i);
             board.append(newLine);
@@ -75,7 +76,7 @@ public class StringGenerator {
         int width = fields[0].length;
         StringBuilder header = new StringBuilder("  ");
         for (int i = 1; i <= width; i++) {
-            String element = (i < 10) ? "  " + "\u001B[35m" + i + " " : " " + "\u001B[35m" + i + " ";
+            String element = (i < 10) ? "  " + YELLOW + i + " " : " " + YELLOW + i + " ";
             header.append(element);
         }
         return header.append("\n");
